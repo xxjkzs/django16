ccd92605848d8647c7d547f1b62532edf8bbc845
xxjkzs <xxjkzs@foxmail.com>
2017-05-24 22:59:01 +0800

    page59 done!

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.template.loader import get_template
+from django.http import HttpResponse
+# from django.shortcuts import render
+from datetime import datetime
+from .models import Post
 
 # Create your views here.
+
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
