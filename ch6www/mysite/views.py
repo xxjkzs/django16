772150713f38447a19e7437d193d0d0c59c023e1
xxjkzs <xxjkzs@foxmail.com>
2018-04-19 21:24:29 +0800

    page154

--- ch6www/mysite/views.py
+++ ch6www/mysite/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.template.loader import get_template
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+	template = get_template('index.html')
+	html = template.render()
+	return HttpResponse(html)
