d320b9cc9f78e584a2c67567fce6e4d0e401ebf9
xxjkzs <xxjkzs@foxmail.com>
2018-05-06 14:19:38 +0800

    Blame everything on the dog.

--- ch8/mysite/views.py
+++ ch8/mysite/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.template.loader import get_template
+from django.http import HttpResponse
+from mysite import models
 # Create your views here.
+
+def index(request):
+	years = range(1960,2020)
+	ufcolor = request.GET.getlist('fcolor')
+	template = get_template('index.html')
+	try:
+		uid = request.GET['user_id']
+		upass = request.GET['user_pass']
+	except:
+		uid = None
+	if uid != None and upass == '123456':
+		verified = True
+	else:
+		verified = False
+	html = template.render(locals())
+	return HttpResponse(html)
