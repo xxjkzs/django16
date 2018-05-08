2ac651d982021e4e7c3b257377f2daaabc870b34
xxjkzs <xxjkzs@foxmail.com>
2018-05-08 20:57:48 +0800

    moved forward one more step.

--- ch8/mysite/views.py
+++ ch8/mysite/views.py
@@ -8,6 +8,8 @@ def index(request):
 	years = range(1960,2020)
 	ufcolor = request.GET.getlist('fcolor')
 	template = get_template('index.html')
+	posts = models.Post.objects.filter(enabled=True).order_by('-pub_time')[:30]
+	moods = models.Mood.objects.all()
 	try:
 		uid = request.GET['user_id']
 		upass = request.GET['user_pass']
