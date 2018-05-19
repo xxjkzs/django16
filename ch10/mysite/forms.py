782b852c0eeffe6f3d4c4c958d1676c4c98206e3
xxjkzs <xxjkzs@foxmail.com>
2018-05-19 16:23:47 +0800

    ch10 initiated.

---
+++ ch10/mysite/forms.py
@@ -0,0 +1,60 @@
+from django import forms
+from captcha.fields import CaptchaField
+from mysite import models
+
+class ContactForm(forms.Form):
+	CITY = [
+		['DL','Dali'],
+		['DG','Dongguan'],
+		['ZB','Zibo'],
+		['FD','Fuding'],
+		['SZ','Suzhou'],
+		['NC','NanChang'],
+		['NA','Other']
+	]
+	user_name = forms.CharField(label='Your Name:',max_length=50,initial='Thanos')
+	user_city = forms.ChoiceField(label='Living City:',choices=CITY)
+	user_school = forms.BooleanField(label='In School?',required=False)
+	user_email = forms.EmailField(label='Email Address:')
+	user_message = forms.CharField(label='Your suggestion:',widget=forms.Textarea)
+
+
+class PostForm(forms.ModelForm):
+	captcha = CaptchaField()
+	class Meta:
+		model = models.Post
+		fields = ['mood','nickname','message','del_pass']
+	def __init__(self,*args,**kwargs):
+		super(PostForm,self).__init__(*args,**kwargs)
+		self.fields['mood'].label='Mood'
+		self.fields['nickname'].label='Nickname'
+		self.fields['message'].label='Message'
+		self.fields['del_pass'].label='Password'
+
+
+class LoginForm(forms.Form):
+	username = forms.CharField(label='Your name',max_length=10)
+	password = forms.CharField(label='Password',widget=forms.PasswordInput())
+
+
+class DateInput(forms.DateInput):
+	input_type = 'date'
+
+
+class DiaryForm(forms.ModelForm):
+
+	class Meta:
+		model = models.Diary
+		# fields = ['user','budget','weight','note','ddate']
+		fields = ['budget','weight','note','ddate']
+		widgets = {
+			'ddate':DateInput(),
+		}
+
+		def __init__(self,*args,**kwargs):
+			super(DiaryForm,self).init(*args,**kwargs)
+			# self.fields['user'].label='User'
+			self.fields['budget'].label='Expenses'
+			self.fields['weight'].label='Weight'
+			self.fields['note'].label='Notes'
+			self.fields['ddate'].label='Date'
---
+++ ch10/mysite/views.py
@@ -0,0 +1,103 @@
+from django.shortcuts import render,redirect
+from django.template.loader import get_template
+from django.template import RequestContext
+from django.http import HttpResponse,HttpResponseRedirect
+from django.core.mail import EmailMessage
+from django.contrib import messages , auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from mysite import models,forms
+# Create your views here.
+
+def index(request,pid=None,del_pass=None):
+	if request.user.is_authenticated():
+		username = request.user.username
+		useremail = request.user.email
+		try:
+			user = models.User.objects.get(username=username)
+			diaries = models.Diary.objects.filter(user=user).order_by('-ddate')
+		except:
+			pass
+	
+	template = get_template('index.html')
+	request_context = RequestContext(request)
+	request_context.push(locals())
+	html = template.render(request_context)
+	return HttpResponse(html)
+
+def login(request):
+	if request.method == 'POST':
+		login_form = forms.LoginForm(request.POST)
+		if login_form.is_valid():
+			login_name = request.POST['username'].strip()
+			login_pass = request.POST['password']
+			user = authenticate(username=login_name,password=login_pass)
+			if user is not None:
+				if user.is_active:
+					auth.login(request,user)
+					print("Successs!")
+					messages.add_message(request,messages.SUCCESS,'Logged in.')
+					return redirect('/')
+				else:
+					messages.add_message(request,messages.WARNING,'User is not activated.')
+			else:
+				messages.add_message(request,messages.WARNING,'Login failed.')
+		else:
+			messages.add_message(request,messages.INFO,'Check your input.')
+	else:
+		login_form = forms.LoginForm()
+
+	template = get_template("login.html")
+	request_context = RequestContext(request)
+	request_context.push(locals())
+	html = template.render(request_context)
+	response = HttpResponse(html)
+	return response
+
+def logout(request):
+	auth.logout(request)
+	messages.add_message(request,messages.INFO,'Logged out.')
+	return redirect('/')
+
+
+@ login_required(login_url='/login/')
+def profile(request):
+	if request.user.is_authenticated():
+		username = request.user.username
+	try:
+		user = User.objects.get(username=username)
+		profile = models.Profile.objects.get(user=user)
+	except:
+		pass
+	template = get_template('profile.html')
+	html = template.render(locals())
+	return HttpResponse(html)
+
+
+@ login_required(login_url='/login/')
+def write(request):
+	if request.user.is_authenticated():
+		username = request.user.username
+		useremail = request.user.email
+	messages.get_messages(request)
+
+	if request.method == 'POST':
+		user = User.objects.get(username=username)
+		diary = models.Diary(user=user)
+		post_form = forms.DiaryForm(request.POST,instance=diary)
+		if post_form.is_valid():
+			messages.add_message(request,messages.INFO,'Saved')
+			post_form.save()
+			return redirect('/')
+		else:
+			messages.add_message(request,messages.INFO,'All fields must be filled.')
+	else:
+		post_form = forms.DiaryForm()
+		messages.add_message(request,messages.INFO,'All fiels must be filled.')
+
+	template = get_template('write.html')
+	request_context = RequestContext(request)
+	request_context.push(locals())
+	html = template.render(request_context)
+	return HttpResponse(html)
