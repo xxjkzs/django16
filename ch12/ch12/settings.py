ee33232a80f0816223c05701f4736908a0bd197b
xxjkzs <xxjkzs@foxmail.com>
2018-07-02 22:23:58 +0800

    django-filer installed , not configged properly.

--- ch12/ch12/settings.py
+++ ch12/ch12/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = (
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
+    'easy_thumbnails',
+    'filer',
+    'mptt',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -126,3 +129,54 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
+
+THUMBNAIL_HIGH_RESOLUTION = True
+
+THUMBNAIL_PROCESSORS = [
+    'easy_thumbnails.processors.colorspace',
+    'easy_thumbnails.processors.autocrop',
+    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+    'easy_thumbnails.processors.filers',
+]
+
+FILER_STORAGES = {
+    'public':{
+        'main':{
+            'ENGINE':'filer.storage.PublicFileSystemStorage',
+            'OPTIONS':{
+                'location':'/media/filer',
+                'base_url':'/media/filer'
+            },
+            'UPLOAD_TO':'filer.utils.generate_filename.randomized',
+            'UPLOAD_TO_PREFIX':'filer_public'
+        },
+        'thumbnails':{
+            'ENGINE':'filer.storage.PublicFileSystemStorage',
+            'OPTIONS':{
+                'location':'/media/filer_thumbnails',
+                'base_url':'/media/filer_thumbnails',
+            },
+        },
+    },
+    'private':{
+        'main':{
+            'ENGINE':'filer.storage.PrivateFileSystemStorage',
+            'OPTIONS':{
+                'location':'../smedia/filer',
+                'base_url':'/smedia/filer'
+            },
+            'UPLOAD_TO':'filer.utils.generate_filename.randomized',
+            'UPLOAD_TO_PREFIX':'filer_public'
+        },
+        'thumbnails':{
+            'ENGINE':'filer.storage.PrivateFileSystemStorage',
+            'OPTIONS':{
+                'location':'/smedia/filer_thumbnails',
+                'base_url':'/smedia/filer_thumbnails',
+            },
+        },
+    },
+}
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/media'
--- ch12/ch12/urls.py
+++ ch12/ch12/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from mysite import views
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index),
     url(r'^accounts/',include('allauth.urls')),
+    url(r'^filer/',include('filer.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
